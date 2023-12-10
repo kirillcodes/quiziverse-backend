31e74b6b24f3d8f96c84757232032f210181b2ec
Kirill <kirillusernet@gmail.com>
2023-12-10 22:21:35 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT || 5000);
+  const PORT = process.env.PORT || 5000;
+
+  await app.listen(PORT);
+  console.log('Server was started on port: ' + PORT);
 }
 
 bootstrap();
